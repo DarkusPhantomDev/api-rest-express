aea30d214a784efd7b89153b66fa6fd68d3d5656
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-10-24 23:03:48 -0400

    Routing con Express.js

--- index.js
+++ index.js
@@ -7,6 +7,37 @@ app.get('/', (request, response) => {
   response.send("Hola. Acabas de iniciar el server en express");
 })
 
+app.get('/nueva-ruta', (request, response) => {
+  response.send("Hola. Estas en nueva ruta");
+})
+
+app.get('/products', (request, response) => {
+  //response.send("Hola. Estas en productos");
+  response.json({
+    name: 'Producto 1',
+    price: 1899,
+  })
+})
+
+app.get('/home', (request, response) => {
+  response.send(`<h1>Usted esta en el Home. Bienvenido, usuario</h1>`);
+})
+
+app.get('/categorias', (request, response) => {
+  response.json({
+    title: "Usted se encuentra en categorias",
+    description: "Acá encontrarás los diversos productos para ti, agrupados en categorias"
+    /*productos: 
+      id: 1,
+      name: "books",
+      id: 2,
+      name: "games",
+      id: 3,
+      name: "courses",
+    }*/
+  });
+})
+
 app.listen(port, () => {
   console.log("El port es:" + port);
 })
