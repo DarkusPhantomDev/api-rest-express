b5ad10bae363f8be3af7939d58459c1b975d2669
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-10-24 22:44:01 -0400

    Instalando Express y abriendo el primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Dorime')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send("Hola. Acabas de iniciar el server en express");
+})
+
+app.listen(port, () => {
+  console.log("El port es:" + port);
+})
