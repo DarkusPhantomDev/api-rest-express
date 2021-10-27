a76f1125934aae4c1038ef11c7a4d69369598985
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-10-27 15:59:19 -0400

    Organizando archivos para ajustarlo a los servicios y creando nuestro servicios

--- app/routes/products.router.js
+++ app/routes/products.router.js
@@ -1,26 +1,11 @@
 const express = require('express');
-const faker = require('faker') ;
+const ProductsService = require('./../services/product.service') ;
 
-//Este señor se usa para crear routers
-//Ojo, esto no quiere decir que ya estemos definiendolo. Eso esta definido en index.js (en la carpeta products)
 const router = express.Router();
+const service = new ProductsService();
 
-//Se eliminaron products en el primer parametro y se dejo todo lo que iba depues de /products
-//Esto porque vamos a definirlo mas adelante y necesitamos las otras areas
 router.get('/', (request, response) => {
-  const products = [];
-  const { size } = request.query;
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      id: index+1,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price()),
-      image: faker.image.imageUrl(),
-    });
-  }
-
+  const products = service.find();
   response.json(products);
 })
 
@@ -30,17 +15,9 @@ router.get('/filter', (request, response) => {
 
 router.get('/:id', (request, response) => {
   const { id } = request.params;
+  const product = service.findOne(id);
 
-  if (id === '999') {
-    response.status(404).json({
-      message: 'not found'
-    })
-  } else {
-    response.status(200).json({
-      id,
-      name: "Mario Kart",
-    })
-  }
+  response.json(product);
 })
 
 router.post('/', (request, response) => {
@@ -51,7 +28,6 @@ router.post('/', (request, response) => {
   });
 })
 
-//El patch siempre recibe un id
 router.patch('/:id', (request, response) => {
   const body = request.body;
   const { id } = request.params;
---
+++ app/services/product.service.js
@@ -0,0 +1,44 @@
+//En este archivo se define toda la logica que va a gestionar los datos
+const faker = require('faker');
+
+class ProductsService {
+  constructor(){
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price()),
+        image: faker.image.imageUrl(),
+      });
+    }
+  }
+
+  create() {
+  
+  }
+  
+  find() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find(item => item.id === id);
+  }
+
+  update() {
+  
+  }
+
+  delete() {
+  
+  }
+}
+
+module.exports = ProductsService;
